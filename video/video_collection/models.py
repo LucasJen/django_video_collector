1b509c7f6acd71cb1ccd371068803621322f0744
LucasJen <fake@fake.com>
2026-04-20 14:49:55 -0500

    added video upload and search

--- video/video_collection/models.py
+++ video/video_collection/models.py
@@ -1,3 +1,36 @@
 from django.db import models
+from urllib import parse
+from django.core.exceptions import ValidationError
 
-# Create your models here.
+class Video(models.Model):
+    # Create model to store video uploads
+    name = models.CharField(max_length=200)
+    url = models.CharField(max_length=400)
+    notes = models.TextField(blank=True, null=True)
+    video_id = models.CharField(max_length=40, unique=True)
+
+    def save(self, *args, **kwargs): #override save to be able to extract video id from url
+        
+        # Validates that the url is a youtube url
+        if not self.url.startswith('https://www.youtube.com/watch'):
+            raise ValidationError(f'Not a Youtube URL: {self.url}')
+        
+        url_components = parse.urlparse(self.url)
+        query_string = url_components.query # i.e  v=1235235
+        if not query_string:
+            raise ValidationError('Invalid YouTube URL {self.url}')
+        parameters = parse.parse_qs(query_string, strict_parsing=True) # sets up dictionary strict parsing ensure valid query string
+        v_parameters_list = parameters.get('v') # looks for dictionary "v" key
+        if not v_parameters_list: # Checking if None or empty list
+            raise ValidationError(f'Invalid Youtube URL, missing parameters {self.url}')
+        self.video_id = v_parameters_list[0] 
+
+        super().save(*args, **kwargs)
+
+            
+
+
+    def __str__(self):
+        return f'ID: {self.pk}, Name: {self.name}, URL: {self.url}, Video ID: {self.video_id}, Notes: {self.notes[:200]}'
+
+    
--- video/video_collection/views.py
+++ video/video_collection/views.py
@@ -1,3 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib import messages
+from django.core.exceptions import ValidationError
+from django.db import IntegrityError
+from django.db.models.functions import Lower
+from .forms import VideoForm, SearchForm
+from .models import Video
 
-# Create your views here.
+
+def home(request):
+    app_name = 'Video Upload Site'
+    return(render(request, 'video_collection/home.html', {app_name: 'app_name'}))
+
+def add(request):
+
+    if request.method == "POST":
+        new_video_form = VideoForm(request.POST) # Creates variable that stores the POST request data for VideoForm
+        if new_video_form.is_valid(): # Uses built in is_valid function to ensure request information is valid
+            try:
+                new_video_form.save()
+                return redirect('video_list')
+                # messages.info(request, 'New video saved!')
+            except ValidationError: # User facing error
+                messages.warning(request, 'Invalid Youtube URL.')
+            except IntegrityError: # User facing error if video is already added.
+                messages.warning(request, 'This video has already been added.')
+
+         # if the form is not valid, it will returnt he same page with the user provided information still in the boxes
+        messages.warning(request, 'Please check the data entered.')
+        return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})         
+    
+    new_video_form = VideoForm()
+    return render(request, 'video_collection/add.html', {'new_video_form': new_video_form})
+
+def video_list(request):
+
+    search_form = SearchForm(request.GET) # create form from data that the user provides
+    if search_form.is_valid():
+        search_term = search_form.cleaned_data['search_term']
+        videos = Video.objects.filter(name__icontains=search_term).order_by(Lower('name')) #orders data by name
+
+    else: # for is not filled in or this the first time the user has visited
+        search_form = SearchForm()
+        videos = Video.objects.order_by(Lower('name'))
+
+    return render(request, 'video_collection/video_list.html', {'videos': videos, 'search_form': search_form})
